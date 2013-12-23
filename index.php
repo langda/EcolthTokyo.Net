6b17fec2da8620ca6896a3370d5cfa08c41fd57e
JameSolution <samedy.jamesolution@gmail.com>
2013-12-24 00:31:41 +0700

    test commit

--- index.php
+++ index.php
@@ -191,6 +191,8 @@ if (defined('ENVIRONMENT'))
 		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
 
+
+
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
